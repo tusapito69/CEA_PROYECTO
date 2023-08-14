3ed18124060b0e22a16b38a5b04b86baa6795750
unknown <lusho18fernando@gmail.com>
2023-08-14 18:02:51 -0400

    l16

--- FRONTEND_CEA/src/app/core/services/actividad.service.ts
+++ FRONTEND_CEA/src/app/core/services/actividad.service.ts
@@ -9,16 +9,16 @@ import { IActividad } from '../interfaces/actividad';
 })
 export class ActividadService {
 
-  private ruta:string="/api/Institucion";
+  private ruta:string="/api/Activity";
   usuario!:any;
 
   constructor(private httpclient:HttpClient) {} 
   obtenerActividades():Observable <any>{
-    return this.httpclient.get<any>(`${environment.API_URL}/api/Actividad`);
+    return this.httpclient.get<any>(`${environment.API_URL}/api/Activity`);
    
   }
   agregarActividad(actividad:IActividad):Observable<IActividad>{
-    return this.httpclient.post<IActividad>(`${environment.API_URL}/api/Actividad`,actividad);
+    return this.httpclient.post<IActividad>(`${environment.API_URL}/api/Activity`,actividad);
   }
   editarActividad(id:number,actividad:IActividad):Observable<any>{
     return this.httpclient.put<any>(`${environment.API_URL}${this.ruta}/${id}`,actividad);
@@ -28,9 +28,9 @@ export class ActividadService {
     );
   }
   obtenerActivos():Observable<any>{
-    return this.httpclient.get<any>(`${environment.API_URL}/api/Actividad/obtenerActivos`);
+    return this.httpclient.get<any>(`${environment.API_URL}/api/Activity/obtenerActivos`);
   }
   obtenerTotal(){
-    return this.httpclient.get(`${environment.API_URL}/api/Actividad/total`);
+    return this.httpclient.get(`${environment.API_URL}/api/Activity/total`);
   }
 }
